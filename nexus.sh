248224e7dd6444689a73f88de32eb41438d485cc
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-08-07 12:41:27 +0530

    Update nexus.sh

--- nexus.sh
+++ nexus.sh
@@ -1,3 +1,7 @@
+sudo su
+ex: 
+root@ip-172-31-88-139:/home/ubuntu#
+ 
 apt-get update -y
 
 echo "Install Java"
@@ -7,12 +11,25 @@ java -version
 echo "Install Nexus"
 useradd -M -d /opt/nexus -s /bin/bash -r nexus
 echo "nexus ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/nexus
+
 mkdir /opt/nexus
 wget https://sonatype-download.global.ssl.fastly.net/repository/downloads-prod-group/3/nexus-3.29.2-02-unix.tar.gz
+
 tar xzf nexus-3.29.2-02-unix.tar.gz -C /opt/nexus --strip-components=1
 chown -R nexus:nexus /opt/nexus
 
-nano /opt/nexus/bin/nexus.vmoptions
+vi /opt/nexus/bin/nexus.vmoptions
+#in The file 
+# ../sonatype-
+# need to change to 
+# ./sonatype- in 4 places; wq!
+------------
+ vi  /opt/nexus/bin/nexus.rc
+
+#run_as_user=""
+to 
+run_as_user="nexus"
+:wq!
 
 
 
