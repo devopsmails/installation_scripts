3f768912cc01392acc7596b0ecf95ef5cdd84cda
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-08-07 11:54:15 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -7,12 +7,14 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" -y
 
+sudo apt upgrade -y
+
 sudo apt update -y
 
 apt-cache policy docker-ce -y
 
 sudo apt install docker-ce -y
 
-#sudo systemctl status docker
-
 sudo chmod 777 /var/run/docker.sock
+
+sudo systemctl status docker
