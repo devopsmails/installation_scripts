f4a8736a1ea82a884ad1b4ab020da50dc60e40ac
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-08-09 18:25:50 +0530

    Update helm_datree.sh

--- helm_datree.sh
+++ helm_datree.sh
@@ -7,7 +7,7 @@ chmod 700 get_helm.sh
 ./get_helm.sh
 
 # Datree.io Install 
-
+apt-get install unzip
 curl https://get.datree.io | /bin/bash
 datree version
 datree test <your_kubernetes_manifest_YAML_NAME>
